000eec5befb82d1d69a5a745446bee8953718cff
SousaEmerson <emersonbdsousa@gmail.com>
2022-07-11 18:53:19 -0300

    first commit

---
+++ knexfile.js
@@ -0,0 +1,36 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+/**module.exports = {
+  client: 'postgresql',
+  connection: {
+    database: 'knowledge',
+    user: 'postgres',
+    password: '123456'
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations'
+  }
+}
+*/
+module.exports = {
+  client: 'pg',
+  connection: {
+    database: 'knowledge',
+    user: 'postgres',
+    password: 'senha'
+  },
+  pool: {
+    min: 2,
+    max: 20
+  },
+  migrations: {
+    tableName: 'knex_migrations'
+  }
+}
